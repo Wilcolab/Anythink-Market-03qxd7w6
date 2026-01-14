3ace3e38698d14512b3a8c01df387ca3246fbe15
WilcoApp <hello@wilco.gg>
2026-01-14 15:18:24 +0000

    Initial commit

--- api/routes.js
+++
@@ -1,5 +0,0 @@
-module.exports = function (app) {
-  const arithmetic = require('./controller');
-  app.route('/arithmetic').get(arithmetic.calculate);
-  app.route('/feedback').get(arithmetic.getFeedbacks).post(arithmetic.submitFeedback);
-};
